f31dd0307e489a566b84383a3f297906e02b3169
DidierLLantenPc <didiervelez@hotmail.es>
2022-03-08 15:47:13 -0500

    Adicion de metodos verificacion

--- src/parcialuno/ParcialUno.java
+++ src/parcialuno/ParcialUno.java
@@ -17,25 +17,47 @@ public class ParcialUno {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         boolean numeros = true;
         do {
             try {
                 String nombre = JOptionPane.showInputDialog("Ingrese el nombre del estudiante: ");
-                double codigo = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el codigo del estudiante: "));
-                double nota1 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la nota del primer Corte: "));
-                double nota2 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la nota del segundo Corte: "));
-                double nota3 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la nota del Tercer Corte: "));
+                if (!nombre.isEmpty()) {
+                    Double codigo = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el codigo del estudiante: "));
+                    if (validarNumero(codigo)) {
+                        double nota1 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la nota del primer Corte: "));
+                        if (validarNumero(nota1)) {
+                            double nota2 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la nota del segundo Corte: "));
+                            if (validarNumero(nota2)) {
+                                double nota3 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la nota del Tercer Corte: "));
+                                if (validarNumero(nota3)) {
+                                    mostrarMensaje("Nombre: " + nombre + "\n"
+                                            + "Codigo: " + codigo + "\n"
+                                            + "\nNota definitiva: " + calcularNotaFinal(nota1, nota2, nota3));
+                                } else {
+                                    mostrarMensaje("Debe ingresar un numero positivo." + "\n" + "Se reiniciara el formulario.");
+                                }
+                            } else {
+                                mostrarMensaje("Debe ingresar un numero positivo." + "\n" + "Se reiniciara el formulario.");
+                            }
 
-                mostrarMensaje("Nombre: "+nombre + "\n"
-                        + "Codigo: " + codigo + "\n"
-                        + "\nNota definitiva: " + calcularNotaFinal(nota1, nota2, nota3));
+                        } else {
+                            mostrarMensaje("Debe ingresar un numero positivo." + "\n" + "Se reiniciara el formulario.");
+                        }
+
+                    } else {
+                        mostrarMensaje("Debe ingresar un numero positivo." + "\n" + "Se reiniciara el formulario.");
+                    }
+
+                } else {
+                    mostrarMensaje("Debe ingresar un nombre.");
+                    numeros = false;
+                }
 
             } catch (NumberFormatException | NullPointerException ex) {
                 if (ex.getMessage() == null) {
                     System.exit(0);
                 }
-                mostrarMensaje("Este campo solo acepta valores numericos.");
+                mostrarMensaje("Este campo solo acepta valores numericos." + "\n" + "Se reiniciara el formulario.");
                 numeros = false;
             }
         } while (numeros == false);
@@ -48,5 +70,9 @@ public class ParcialUno {
 
     private static void mostrarMensaje(String mensaje) {
         JOptionPane.showMessageDialog(null, mensaje);
+    }
+
+    private static boolean validarNumero(double num) {
+        return num > 0;
     }
 }
